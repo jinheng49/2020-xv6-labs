bbbe2006205733d77e939c5094209cb0892b7360
jinheng49 <3172951601@qq.com>
2024-11-13 20:57:00 +0800

    Pass Lab_COW

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -8,6 +8,15 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+// #include <c++/v1/__system_error/errc.h>
+
+
+#define PA2PGREF_ID(p) (((p)-KERNBASE)/PGSIZE)
+#define PGREF_MAX_ENTRIES PA2PGREF_ID(PHYSTOP)
+
+int pageref[PGREF_MAX_ENTRIES];
+
+#define PA2PGREF(p) pageref[PA2PGREF_ID((uint64)(p))]
 
 void freerange(void *pa_start, void *pa_end);
 
@@ -22,11 +31,13 @@ struct {
   struct spinlock lock;
   struct run *freelist;
 } kmem;
+struct spinlock pgreflock;
 
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&pgreflock, "pgref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -51,15 +62,19 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&pgreflock);
+  if(--PA2PGREF(pa) <= 0 ){
   // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }
+  release(&pgreflock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -76,7 +91,38 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    PA2PGREF(r) = 1;
+  }
   return (void*)r;
+}
+
+void *
+kcopy(void *pa){
+  acquire(&pgreflock);
+
+  if(PA2PGREF(pa) <= 1){
+    release(&pgreflock);
+    return pa;
+  }
+
+  uint64 newpg = (uint64)kalloc();
+  if(newpg == 0){
+    release(&pgreflock);
+    return 0;
+  }
+  memmove((void*)newpg, (void*)pa, PGSIZE);
+
+  PA2PGREF(pa)--;
+  
+  release(&pgreflock);
+  return (void*)newpg;
+}
+
+void
+krefpage(void *pa) {
+  acquire(&pgreflock);
+  PA2PGREF(pa)++;
+  release(&pgreflock);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -303,7 +305,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+  // char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -311,14 +313,28 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    // flags = PTE_FLAGS(*pte);
+
+    // if(*pte & ~PTE_COW){
+    //   *pte &= ~PTE_W;
+    //   *pte |= PTE_COW;
+    // }
+    if(*pte & PTE_W) {
+      *pte = (*pte & ~PTE_W) | PTE_COW;
+    }
+    
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    if(mappages(new,i,PGSIZE,(uint64)pa,flags) != 0){
       goto err;
     }
+    krefpage((void*) pa);
+    // if((mem = kalloc()) == 0)
+    //   goto err;
+    // memmove(mem, (char*)pa, PGSIZE);
+    // if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+    //   kfree(mem);
+    //   goto err;
+    // }
   }
   return 0;
 
@@ -349,6 +365,8 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   uint64 n, va0, pa0;
 
   while(len > 0){
+    if(uvmcheckcowpage(dstva)) 
+      uvmcowcopy(dstva);
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
@@ -431,4 +449,36 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+int 
+uvmcheckcowpage(uint64 va) {
+  pte_t *pte;
+  struct proc *p = myproc();
+  
+  return va < p->sz 
+    && ((pte = walk(p->pagetable, va, 0))!=0)
+    && (*pte & PTE_V) 
+    && (*pte & PTE_COW); 
+}
+
+int
+uvmcowcopy(uint64 va){
+  pte_t *pte;
+  struct proc *p = myproc();
+
+  if((pte = walk(p->pagetable, va, 0)) == 0)
+    panic("uvmcowcopy: walk");
+  
+  uint64 pa = PTE2PA(*pte);
+  uint64 new = (uint64)kcopy((void*)pa);
+  if(new == 0) 
+    return -1;
+  uint64 flags = (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW;
+  uvmunmap(p->pagetable, PGROUNDDOWN(va), 1, 0);
+  if(mappages(p->pagetable, va, 1, new, flags) == -1) {
+    panic("uvmcowcopy: mappages");
+  }
+  return 0;
+
 }
